81d3a38f6126c2df8cc9a52c42c055031062fe1a
Zaur <you@example.com>
2026-02-19 14:23:12 +0300

    created urlDecoded AND urlUndecoder

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,16 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+
+	"github.com/lxmp7p/yaGo-url-shortener/internal/handler"
+)
+
+func main() {
+	http.HandleFunc(`/`, handler.Handler)
+
+	err := http.ListenAndServe(`:8080`, nil)
+	if err != nil {
+		panic(err)
+	}
+}
