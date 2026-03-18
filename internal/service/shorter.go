3ff369641d9e929c3f0bb2ede88b4e21a937eb5c
Zaur <you@example.com>
2026-03-18 15:07:18 +0300

    fix header

--- internal/service/shorter.go
+++ internal/service/shorter.go
@@ -49,15 +49,20 @@ func (sr *ShortenRequest) Bind(r *http.Request) error {
 
 func (s *ShortenerService) GetShortURLApi(res http.ResponseWriter, req *http.Request) {
 	var shortenRequest ShortenRequest
+	fmt.Println("1")
 
 	if err := json.NewDecoder(req.Body).Decode(&shortenRequest); err != nil {
+		fmt.Println("2")
 		http.Error(res, "Invalid JSON", http.StatusBadRequest)
 		return
 	}
 	if err := shortenRequest.Bind(req); err != nil {
+		fmt.Println("3")
+
 		http.Error(res, "Invalid JSON", http.StatusBadRequest)
 		return
 	}
+	fmt.Println("4")
 
 	var shortURL string
 	for attempt := 0; attempt < MaxShortAttempts; attempt++ {
@@ -68,6 +73,7 @@ func (s *ShortenerService) GetShortURLApi(res http.ResponseWriter, req *http.Req
 		}
 		break
 	}
+	fmt.Println("5")
 
 	res.Header().Set("Content-Type", "application/json")
 	res.WriteHeader(http.StatusCreated)
