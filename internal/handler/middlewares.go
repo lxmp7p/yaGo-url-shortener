0be2becd96f7261c1e32410d0c3df3c24a4fce98
ZRasulov <zrasulov@skala-r.ru>
2026-03-20 11:02:06 +0300

    try fix

--- internal/handler/middlewares.go
+++ internal/handler/middlewares.go
@@ -96,6 +96,12 @@ func CompressMiddleware() func(http.Handler) http.Handler {
 				defer gz.Close()
 			}
 
+			acceptEncoding := r.Header.Get(AcceptEncoding)
+			if acceptEncoding == "" || !strings.Contains(acceptEncoding, "gzip") {
+				h.ServeHTTP(w, r)
+				return
+			}
+
 			var cw *compressResponseWriter
 			if strings.Contains(r.Header.Get(AcceptEncoding), Gzip) {
 				cw = &compressResponseWriter{ResponseWriter: w}
