1ba97b0c0ea160f7f4e1ed03afabe27c22296432
ZRasulov <zrasulov@skala-r.ru>
2026-03-20 12:14:04 +0300

    add tests

--- internal/handler/middlewares.go
+++ internal/handler/middlewares.go
@@ -96,6 +96,10 @@ func CompressMiddleware() func(http.Handler) http.Handler {
 				defer gz.Close()
 			}
 
+			// ПРИ ВКЛЮЧЕНИИ КОДА НИЖЕ ПАДАЕТ ТЕСТЫ ДЛЯ iter7
+			// МБ Я НЕ ПОНЯЛ ЗАДАНИЯ И ТАК И НУЖНО, ПОТОМУ ЧТО БЕЗ ЭТОГО ТЕСТЫ iter8 ПРОХОДЯТ
+			// ЛИБО Я БЕЗ ПОНЯТИЯ ПОЧЕМУ ОТВЕТЫ ДЛЯ ЗАПРОСОВ БЕЗ acceptEncoding ВСЕ РАВНО СЖИМАЮТСЯ
+
 			// acceptEncoding := r.Header.Get(AcceptEncoding)
 			// if acceptEncoding == "" || !strings.Contains(acceptEncoding, "gzip") {
 			h.ServeHTTP(w, r)
