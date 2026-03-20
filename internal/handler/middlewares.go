bac8b00e744349dc54abfcf01394e6e1a12a0f95
ZRasulov <zrasulov@skala-r.ru>
2026-03-20 11:11:31 +0300

    try fix

--- internal/handler/middlewares.go
+++ internal/handler/middlewares.go
@@ -47,6 +47,22 @@ type compressResponseWriter struct {
 	Writer *gzip.Writer
 }
 
+func (compressWriter *compressResponseWriter) Write(b []byte) (int, error) {
+	contentType := compressWriter.Header().Get(ContentType)
+	if compressWriter.Writer == nil {
+		compressWriter.Header().Set("Content-Encoding", "gzip")
+		if strings.Contains(contentType, AppJSON) || strings.Contains(contentType, TextHTML) {
+			compressWriter.Writer = gzip.NewWriter(compressWriter.ResponseWriter)
+		}
+	}
+
+	if compressWriter.Writer != nil {
+		return compressWriter.Writer.Write(b)
+	}
+
+	return compressWriter.ResponseWriter.Write(b)
+}
+
 func LoggingMiddleware(logger *logrus.Logger) func(http.Handler) http.Handler {
 	return func(h http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -81,7 +97,6 @@ func CompressMiddleware() func(http.Handler) http.Handler {
 			}
 
 			h.ServeHTTP(w, r)
-			return
 		})
 	}
 }
