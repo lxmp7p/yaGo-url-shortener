a0f55d0d421f3d1e34d08fe9e9de42c04e240f2a
ZRasulov <zrasulov@skala-r.ru>
2026-04-03 16:46:23 +0300

    add db

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -61,10 +61,10 @@ func runMigrations(DSN string, logger *logrus.Logger) {
 	dbURL := DSN
 	m, err := migrate.New(migrationsPath, dbURL)
 	if err != nil {
-		logger.Error("Failed to initialize migrate: %v", err)
+		logger.Errorf("Failed to initialize migrate: %v", err)
 	}
 	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
-		logger.Error("Migration failed: %v", err)
+		logger.Errorf("Migration failed: %v", err)
 	}
 
 	logger.Println("Migrations applied successfully!")
